443a7c5abfc21afc1c49ffd49145cc59ed8f8a05
Alone <lieyan_alone@hotmail.com>
2026-07-26 22:59:37 +0800

    base item and curios

---
+++ src/main/java/Alone818/com/bread_of_amx/itemcuiros/driest_bread.java
@@ -0,0 +1,9 @@
+package Alone818.com.bread_of_amx.itemcuiros;
+
+import net.minecraft.world.item.Item;
+
+public class driest_bread extends Item {
+    public driest_bread(Properties properties) {
+        super(properties);
+    }
+}
